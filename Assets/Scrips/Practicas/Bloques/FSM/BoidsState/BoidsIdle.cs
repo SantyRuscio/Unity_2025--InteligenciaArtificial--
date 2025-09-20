c8394a7fe36f72a8887e6ea8f1cd273ea77d0eec
SantyRuscio <santiruscio1237@gmail.com>
2025-09-20 19:28:30 -0300

    Agregados Decorativos y Audio

--- Assets/Scrips/Practicas/Bloques/FSM/BoidsState/BoidsIdle.cs
+++ Assets/Scrips/Practicas/Bloques/FSM/BoidsState/BoidsIdle.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 
 public class BoidsIdle : BaseState
 {
-    //private Animator _animator;
+    private Animator _animator;
 
     public override void OnEnter()
     {
-        Debug.Log("PREY : IDLE");
+        Debug.Log("PREY: IDLE");
 
-       // if (_animator != null)
-       //     _animator.SetBool("Walk", false); // 
-       //
-        // apenas entra en Idle, cambia a Patrol
         fsm.ChnageState(AgentStates.Patrol);
+
+    }
+
+    public override void OnUpdate()
+    {
+
     }
 
     public override void OnExit()
     {
-        Debug.Log("PREY: sali de idle");
+
     }
 }
+
+
